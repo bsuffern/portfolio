218b20264e7258898dd22c1df9bde69e40325c10
Broden Suffern <broden.suffern24@gmail.com>
2024-08-22 22:11:21 +1000

    Initial commit

---
+++ src/components/FormattedDate.tsx
@@ -0,0 +1,23 @@
+const dateFormatter = new Intl.DateTimeFormat('en-US', {
+  year: 'numeric',
+  month: 'short',
+  day: 'numeric',
+  timeZone: 'UTC',
+})
+
+export function FormattedDate({
+  date,
+  ...props
+}: React.ComponentPropsWithoutRef<'time'> & { date?: string | Date }) {
+  date = typeof date === 'string' ? new Date(date) : date
+
+  if(!date) {
+    return(<></>);
+  }
+
+  return (
+    <time dateTime={date.toISOString()} {...props}>
+      {dateFormatter.format(date)}
+    </time>
+  )
+}
---
+++ src/components/Intro.tsx
@@ -0,0 +1,49 @@
+import Link from 'next/link'
+
+import { IconLink } from '@/components/IconLink'
+
+function GitHubIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
+  return (
+    <svg viewBox="0 0 16 16" aria-hidden="true" fill="currentColor" {...props}>
+      <path d="M8 .198a8 8 0 0 0-8 8 7.999 7.999 0 0 0 5.47 7.59c.4.076.547-.172.547-.384 0-.19-.007-.694-.01-1.36-2.226.482-2.695-1.074-2.695-1.074-.364-.923-.89-1.17-.89-1.17-.725-.496.056-.486.056-.486.803.056 1.225.824 1.225.824.714 1.224 1.873.87 2.33.666.072-.518.278-.87.507-1.07-1.777-.2-3.644-.888-3.644-3.954 0-.873.31-1.586.823-2.146-.09-.202-.36-1.016.07-2.118 0 0 .67-.214 2.2.82a7.67 7.67 0 0 1 2-.27 7.67 7.67 0 0 1 2 .27c1.52-1.034 2.19-.82 2.19-.82.43 1.102.16 1.916.08 2.118.51.56.82 1.273.82 2.146 0 3.074-1.87 3.75-3.65 3.947.28.24.54.73.54 1.48 0 1.07-.01 1.93-.01 2.19 0 .21.14.46.55.38A7.972 7.972 0 0 0 16 8.199a8 8 0 0 0-8-8Z" />
+    </svg>
+  )
+}
+
+function LinkedinIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+      <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z"/>
+    </svg>
+  )
+}
+
+export function Intro() {
+  return (
+    <>
+      <h1 className="mt-auto font-display text-4xl/tight font-light text-white">
+        Broden {' '}
+        <span className="text-sky-300">Suffern</span>
+      </h1>
+      <p className="mt-4 text-sm/6 text-gray-300">
+        Personal portfolio.
+      </p>
+      <div className="mt-8 flex flex-wrap justify-center gap-x-1 gap-y-3 sm:gap-x-2 lg:justify-start">
+        <IconLink href="https://github.com/bsuffern" icon={GitHubIcon} className="flex-none">
+          GitHub
+        </IconLink>
+        <IconLink href="https://www.linkedin.com/in/broden-suffern-680b681aa/" icon={LinkedinIcon} className="flex-none">
+          LinkedIn
+        </IconLink>
+      </div>
+    </>
+  )
+}
+
+export function IntroFooter() {
+  return (
+    <p className="flex items-baseline gap-x-2 text-[0.8125rem]/6 text-gray-500">
+      Powered by Next.js
+    </p>
+  )
+}
